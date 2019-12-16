b39c6ee96ab9374787fda9a1fa1fff1dbe4d56e8
birdalugureren <birdalugureren@gmail.com>
2019-12-16 23:33:42 +0300

    Create 03_Sum_and_Difference_of_Two_Numbers.c

---
+++ 03_Sum_and_Difference_of_Two_Numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+#link of challenge
+# https://www.hackerrank.com/challenges/sum-numbers-c/problem
+
+int main()
+{
+    int d1,d2;
+    float f1,f2;
+
+    scanf("%d %d",&d1,&d2);
+    scanf("%f %f",&f1,&f2);
+
+    printf("%d %d\n",d1+d2, d1-d2);
+    printf("%0.1f %0.1f", f1+f2, f1-f2); 
+    return 0;
+}
+
